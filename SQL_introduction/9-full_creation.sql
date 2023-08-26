98f0a7f133bdd67f40fd1de4909779f74ecc2c76
vessoutraore <yvtraore94@yahoo.com>
2023-08-26 15:01:06 +0000

    Tasks - 9. Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- create a table
+
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(65),
+    score INT
+);
+
+--insert into the created table
+INSERT INTO second_table (id, name, score)
+    VALUES
+        (1, 'John', 10),
+        (2, 'Alex', 3),
+        (3, 'Bob', 14),
+        (4, 'George', 8);
