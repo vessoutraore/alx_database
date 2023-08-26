fe0b535512323fd07258eb38e325104d9eb21653
vessoutraore <yvtraore94@yahoo.com>
2023-08-26 16:30:28 +0000

    Tasks - 2. Read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- create DB and user and give select priviliges to User
+CREATE DATABASE 
+    IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER 
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT
+    ON  hbtn_0d_2.*
+    TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
